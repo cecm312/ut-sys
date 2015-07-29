2d6fcefd622fc53fa08173ffcc39499234a8ce6c
Carlos <carlos@sacscloud.com>
2015-07-29 13:35:47 -0500

    session actualizad

--- core/mainController.php
+++ core/mainController.php
@@ -2,17 +2,15 @@
 session_start();
 
 $document_root=$_SERVER['DOCUMENT_ROOT'];
-$document_root=str_replace("/","\\",$document_root );
+$document_root=str_replace("\\","/",$document_root );
 $actual_directory=  getcwd();
-$actual_directory=  str_replace("/","\\",$actual_directory );
+$actual_directory=  str_replace("\\","/",$actual_directory );
 $appDir="/".str_replace($document_root,"",$actual_directory)."/";
 define("ROOT_DIR", $appDir);
 define("MODULE_DIR", $appDir."modules/");
 define("LIBS_DIR", $appDir."libs/");
 define("UPLOADS_DIR", $appDir."uploads/");
 define("SITE_DIR", $appDir."site/");
-
+ echo LIBS_DIR;
 $params=$_REQUEST;
-
-$usuario=$_SESSION["user"];
 
